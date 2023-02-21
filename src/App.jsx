aa4addc20f21493694b310f2f0db318e4838752a
Raquel Santos <raquelmrsantos@gmail.com>
2023-02-21 16:59:32 +0000

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// src/App.js
+import { useState } from 'react';
 import './App.css'
+import contacts from './contacts.json'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firstSeven, setFirstSeven] = useState(contacts.slice(0, 7));
+  const [remainingContacts, setRemainingContacts] = useState(contacts.slice(7));
+
+  function addRandomContact() {
+    if (remainingContacts.length > 0) {
+      const random = Math.floor(Math.random() * remainingContacts.length);
+      const pickedContact = remainingContacts.splice(random, 1)[0];
+      setFirstSeven([...firstSeven, pickedContact])
+      console.log(remainingContacts.length)
+    } else {
+      alert("No more contacts")
+    }
+  }
+
+  function sortByName () {
+    setFirstSeven([...firstSeven.sort((a, b) => a.name.localeCompare(b.name))])
+  }
+
+  function sortByPopularity () {
+    setFirstSeven([...firstSeven.sort((a, b) => (b.popularity) - (a.popularity))])
+  }
+
+  function deleteContact (id) {
+    const contactsRemaining = firstSeven.filter((contact) => contact.id !== id)
+    setFirstSeven(contactsRemaining);
+  }
+  
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='App'>
+    <h1>IronContacts</h1>
+      <button onClick={addRandomContact}>Add Random Contact</button>
+      <button onClick={sortByPopularity}>Sort by popularity</button>
+      <button onClick={sortByName}>Sort by name</button>
+    
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an Oscar</th>
+            <th>Won an Emmy</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {firstSeven.map((contact) => (
+          <tr key={contact.id}>
+            <td>
+              <img className ='image' src={contact.pictureUrl} alt="celeb" />
+            </td>
+            <td>
+              {contact.name}
+            </td>
+            <td>
+                {contact.popularity.toFixed(2)}
+            </td>
+              {contact.wonEmmy ? <td><img src='/public/trophy.jpeg' alt ='trophy'></img></td> : <td></td>}
+              {contact.wonOscar ? <td><img src='/public/trophy.jpeg' alt='trophy'></img></td> : <td></td>}
+  
+              <td> <button onClick={() => deleteContact(contact.id)}>Delete</button></td>
+          </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
-
 export default App
+
